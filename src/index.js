bec7ac361b80b5ac33cfd634026ea62a33a81e85
Kautuk Adarsh <kautukadarsh1@gmail.com>
2025-08-29 13:23:25 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const bfhlRoute = require("./routes/bfhl");
 const serverless = require("serverless-http");
-
 const app = express();
+
 app.use(express.json());
 app.use("/", bfhlRoute);
 
 
 module.exports = app;
-module.exports.handler = serverless(app);
